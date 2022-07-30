6f876a510a18bdc6b625ce62ec3f218170f9fa95
TuDZ99 <tubuithanh99@icloud.com>
2022-07-30 19:56:14 +0700

    baitap1

---
+++ src/rikkei/Circle.java
@@ -0,0 +1,40 @@
+package rikkei;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    public Circle(){
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+        public double getArea(){
+        return radius * radius * Math.PI;
+        }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
